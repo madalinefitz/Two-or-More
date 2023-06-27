5e4016e41f17bb298b5a5995b63f95c7b88473a9
Grace Nieboer <grace.nieboer@outlook.com>
2023-06-27 19:37:17 -0400

    change to className and add boolean value to exact paths

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,45 +4,48 @@ import {NavLink} from 'react-router-dom'
 
 function NavBar(){
     return(
-        <nav class="bg-gray-800">
-            <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-                <div class="relative flex h-16 items-center justify-between">
-                    <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                        <div class="hidden sm:ml-6 sm:block">
-                            <div class="flex space-x-4">
-                                <div class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-                                    <NavLink to='/' exact>
+        <nav className="bg-gray-800">
+            <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                <div className="relative flex h-16 items-center justify-between">
+                    <div className="text-gray-300 " >
+                        TWO OR MORE
+                    </div>
+                    <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-end">
+                        <div className="hidden sm:ml-6 sm:block">
+                            <div className="flex space-x-4">
+                                <div className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
+                                    <NavLink to='/' exact='true'>
                                         Home
                                     </NavLink>
                                 </div>
-                                <div class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-                                    <NavLink to='aboutus' exact >
+                                <div className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
+                                    <NavLink to='aboutus' exact='true' >
                                         About Us
                                     </NavLink>
                                 </div>
-                                <div class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-                                    <NavLink to='share' exact >
+                                <div className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
+                                    <NavLink to='share' exact='true' >
                                         Share
                                     </NavLink>
                                 </div>
-                                <div class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-                                    <NavLink to="stories" exact>
+                                <div className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
+                                    <NavLink to="stories" exact='true'>
                                         Stories
                                     </NavLink>
                                 </div>
-                                <div class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-                                    <NavLink to="library" exact>
+                                <div className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
+                                    <NavLink to="library" exact='true'>
                                         Library
                                     </NavLink>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                        <button type="button" class="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                    {/* <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                        <button type="button" className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                             Donate Now
                         </button>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </nav>
