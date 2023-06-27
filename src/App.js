088513dd8f6d76164e993b12ff06df94e63837a4
Grace Nieboer <grace.nieboer@outlook.com>
2023-06-27 19:08:39 -0400

    Stories component created and added to NavBar

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import {Routes, Route} from 'react-router-dom'
 import Home from './components/Home'
 import AboutUs from './components/AboutUs'
 import NavBar from './components/NavBar'
+import Stories from './components/Stories';
 
 function App() {
   return (
     <div >
       <NavBar/>
         <Routes>
-            <Route exact path="/" element={<Home/>}/>
-            <Route exact path="/aboutus" element={<AboutUs />}/>
+            <Route exact path="/" element={<Home/>} />
+            <Route exact path="/stories" element={<Stories />} />
+            <Route exact path="/aboutus" element={<AboutUs />} />
         </Routes>
     </div>
   );
