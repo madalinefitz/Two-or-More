3a451d7f390a830eca651ff9635b48f6ffd0ed1d
madalinefitzpatrick <madaline.fitzpatrick@gmail.com>
2023-06-28 11:53:24 -0400

    styling added

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,13 +1,21 @@
 function AboutUs(){
     return(
         <div className="aboutBackground">
-            <h1>About Us</h1>
-            <iframe src="https://player.vimeo.com/video/791915330?h=4e82b9fac6&color=f7542b& title=0&byline=0&portrait=0" 
-                width="640" 
-                height="360" 
-                frameborder="0" 
-                allow="autoplay; fullscreen; picture-in-picture" 
-                allowfullscreen/>
+            <div className="videoBackground">
+                <div className="titleBox">
+                    <h1 className="title">Founder's Story</h1>
+                    <p className="videoDiscription">Lorem ipsum dolor sit amet consectetur adipiscing elit, massa facilisis convallis consequat primis in nullam urna, penatibus elementum ad scelerisque condimentum maecenas. Odio nunc nullam iaculis fusce praesent fringilla hac cubilia, eleifend id urna mi quis congue sodales, augue viverra nulla nascetur commodo porta risus. Mus quis fermentum odio arcu class dis mattis, sem luctus augue et ac.
+                    </p>
+                </div>
+                <iframe src="https://player.vimeo.com/video/791915330?h=4e82b9fac6&color=f7542b& title=0&byline=0&portrait=0" 
+                    width="640" 
+                    height="360" 
+                    frameborder="0" 
+                    allow="autoplay; fullscreen; picture-in-picture" 
+                    allowfullscreen
+                    className="video"/>
+            </div>
+            
         </div>
     )
 }
