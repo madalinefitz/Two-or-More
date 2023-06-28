76afbbd9bab5066b71a46940c37e368ef313dd56
Grace Nieboer <grace.nieboer@outlook.com>
2023-06-28 11:11:21 -0400

    remove App.css file

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,9 @@ function Home(){
     return (
         <div className="homeBackground">
             <img alt='venn diagram' src={venn} className="venn"/>
+            <div className="flex justify-center">
+                <div className="text-6xl" >Welcome</div>
+            </div>
         </div>
     )
 }
