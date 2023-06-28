a6fd76ae116eed1a2432962207473a117f41cae5
madalinefitzpatrick <madaline.fitzpatrick@gmail.com>
2023-06-28 10:56:23 -0400

    video embedded

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,8 +1,13 @@
-
 function AboutUs(){
     return(
-        <div>
+        <div className="aboutBackground">
             <h1>About Us</h1>
+            <iframe src="https://player.vimeo.com/video/791915330?h=4e82b9fac6&color=f7542b& title=0&byline=0&portrait=0" 
+                width="640" 
+                height="360" 
+                frameborder="0" 
+                allow="autoplay; fullscreen; picture-in-picture" 
+                allowfullscreen/>
         </div>
     )
 }
