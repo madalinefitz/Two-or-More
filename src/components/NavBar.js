38ada855767ffbada4c7acd185dc07ef978a9358
Grace Nieboer <grace.nieboer@outlook.com>
2023-06-27 20:29:57 -0400

    add dividers to NavBar between pages

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,12 +12,13 @@ function NavBar(){
                     </div>
                     <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-end">
                         <div className="hidden sm:ml-6 sm:block">
-                            <div className="flex space-x-4">
+                            <div className="flex space-x-4 divide-x-4 divide-gray-300">
                                 <div className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
                                     <NavLink to='/' exact='true'>
                                         Home
                                     </NavLink>
                                 </div>
+                                
                                 <div className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
                                     <NavLink to='aboutus' exact='true' >
                                         About Us
