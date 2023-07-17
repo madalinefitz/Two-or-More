0404b6db5bf701b1753896d24ae68d98d1e08e06
Grace Nieboer <grace.nieboer@outlook.com>
2023-07-17 17:49:07 -0400

    flexbox and header placeholders

--- src/components/Share.js
+++ src/components/Share.js
@@ -1,7 +1,8 @@
 export default function Share () {
     return(
-        <div>
-            <h1>Share YOUR story</h1>
+        <div className="flex-col p-20 justify-center text-[#E8E4DE] bg-[#292929] bg-cover h-screen">
+            <div className="text-8xl text-right ml-20 p-10">We'd love to learn more about you! This is a safe space to share your experiences, your history, and your journey.</div>
+            <div className="text-8xl w-2/3 p-10">Tell your story.</div>
         </div>
     )
 }
