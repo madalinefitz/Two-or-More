301ba85ea12bf89bd2efff08eb67bcb5d8840ea8
Grace Nieboer <grace.nieboer@outlook.com>
2023-07-17 22:30:55 -0400

    change tab title

--- src/components/Share.js
+++ src/components/Share.js
@@ -1,8 +1,48 @@
 export default function Share () {
     return(
-        <div className="flex-col p-20 justify-center text-[#E8E4DE] bg-[#292929] bg-cover h-screen">
+        <div className="flex-col p-32 justify-center text-[#E8E4DE] bg-[#292929] bg-cover h-full">
             <div className="text-8xl text-right ml-20 p-10">We'd love to learn more about you! This is a safe space to share your experiences, your history, and your journey.</div>
             <div className="text-8xl w-2/3 p-10">Tell your story.</div>
+            <form class="w-full max-w-sm">
+                <div class="md:flex md:items-center mb-6">
+                    <div class="md:w-1/3">
+                    <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+                        First Name
+                    </label>
+                    </div>
+                    <div class="md:w-2/3">
+                    <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="Jane"/>
+                    </div>
+                </div>
+                <div class="md:flex md:items-center mb-6">
+                    <div class="md:w-1/3">
+                    <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+                        Last Initial
+                    </label>
+                    </div>
+                    <div class="md:w-2/3">
+                    <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="D"/>
+                    </div>
+                </div>
+                <div class="md:flex md:items-center mb-6">
+                    <div class="md:w-1/3">
+                    <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password">
+                        Password
+                    </label>
+                    </div>
+                    <div class="md:w-2/3">
+                    <textarea class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="text" placeholder="..."/>
+                    </div>
+                </div>
+                <div class="md:flex md:items-center">
+                    <div class="md:w-1/3"></div>
+                    <div class="md:w-2/3">
+                    <button class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+                        Share
+                    </button>
+                    </div>
+                </div>
+            </form>
         </div>
     )
 }
