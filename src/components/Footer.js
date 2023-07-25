1928425577e30f35e54928403438e5f7973ce1d9
Grace Nieboer <grace.nieboer@outlook.com>
2023-07-25 08:51:58 -0400

    footer component created

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+export default function Footer () {
+    return (
+        <div className="">
+            <div>
+                <div>Our Mission</div>
+                <div>Two Or More Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras placerat nec arcu a molestie. Aenean mollis interdum erat. Praesent neque est, luctus pharetra est non, facilisis dapibus odio. Donec imperdiet, nulla non hendrerit dapibus, leo quam consequat nunc, eu volutpat risus tellus in nisl. Etiam vitae vulputate leo, eleifend hendrerit dolor. Praesent interdum, massa sit amet porta rhoncus, erat massa semper enim, sit amet convallis quam nulla quis risus.  </div>
+            </div>
+
+        </div>
+    )
+}
