b8fc25c6f2970c02f4315dc51fe52b27a87d17c5
madalinefitzpatrick <madaline.fitzpatrick@gmail.com>
2023-07-27 11:48:04 -0400

    placeholder styling for team profile

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -20,6 +20,18 @@ function AboutUs(){
                 </div>
             </div>
             <div className="flex justify-between m-3 mt-8">
+                <div className="text-[#292929] bg-[#E8E4DE] content-center w-2/3  m-2 p-10">
+                    <h1 className="font-bold text-3xl text-center">Meet Karis</h1>
+                    <img src={'https://images.unsplash.com/photo-1457732815361-daa98277e9c8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'} alt="Whitney" className="rounded-full  w-52 h-52 float-left m-5"/>
+                    <p className="m-10 text-xl text-end">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+                <div className="text-[#292929] bg-[#E8E4DE] content-center w-2/3 m-2 p-10">
+                    <h1 className="font-bold text-3xl text-center">Meet Whitney</h1>
+                    <img src={'https://images.unsplash.com/photo-1457732815361-daa98277e9c8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'} alt="Whitney" className="rounded-full  w-52 h-52 float-right m-5"/>
+                    <p className="m-10 text-xl text-start">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p> 
+                </div>
+            </div>
+            <div className="flex justify-between m-3 mt-8">
                 <div className="text-[#D6C6AA] bg-[#292929] content-center w-1/3 text-center text-2xl m-2 p-10">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
                 </div>
