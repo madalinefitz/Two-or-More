02585f15c5fdd3108b81da980711c8328879b50b
madalinefitzpatrick <madaline.fitzpatrick@gmail.com>
2023-08-08 18:59:38 -0400

    lib reading card component added

--- src/components/LibReading.js
+++ src/components/LibReading.js
@@ -1,4 +1,5 @@
 import {useState, useEffect} from "react"
+import LibReadingCard from "./LibReadingCard"
 
 export default function LibReading() {
     const [books, setBooks] = useState([])
@@ -11,22 +12,7 @@ export default function LibReading() {
 
     const bookComponents = books.map(book => {
         return (
-            <div class="max-w-xs rounded overflow-hidden shadow-lg bg-[#292929] mx-20">
-                <img class="w-full max-h-40" src={book.bookCover} alt={`${book.title} cover`}/>
-                <div class="px-6 py-4">
-                    <h1 class="font-bold text-xl mb-2 text-[#E8E4DE]">{book.title}</h1>
-                    <p class="text-[#E8E4DE] text-base">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-                    </p>
-                    <p class="text-[#E8E4DE] text-base">{book.link}</p>
-                </div>
-                <div class="flex items-center">
-                    <img class="w-10 h-10 rounded-full mr-4 p-2" src={book.authorPhoto} alt={book.authorName}/>
-                    <div class="text-sm">
-                        <p class="text-[#E8E4DE] leading-none">{book.authorName}</p>
-                    </div>
-                </div>
-            </div>  
+             <LibReadingCard key={book.id} {...book}/>
         )
     })
 
