c3e74cef1a8638c230c8e6fb23556f7e81b0182e
madalinefitzpatrick <madaline.fitzpatrick@gmail.com>
2023-08-14 22:18:19 -0400

    style changes to share page

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,15 +1,17 @@
 function AboutUs(){
     return(
         <div className="bg-[#D6C6AA] p-10 bg-cover h-auto">
-            <div className="flex justify-between bg-[#292929] p-5">
-                <div className="text-[#D6C6AA] text-2xl m-10">
-                    <div className="content-center m-5">
-                        <h1 className="text-[#D6C6AA] text-4xl">Founder's Story</h1>
-                        <p className="text-[#E8E4DE]">Lorem ipsum dolor sit amet consectetur adipiscing elit, massa facilisis convallis consequat primis in nullam urna, penatibus elementum ad scelerisque condimentum maecenas. Odio nunc nullam iaculis fusce praesent fringilla hac cubilia, eleifend id urna mi quis congue sodales, augue viverra nulla nascetur commodo porta risus. Mus quis fermentum odio arcu class dis mattis, sem luctus augue et ac.
-                        </p>
-                    </div>
-                </div>
+            <div className="flex justify-between m-3 mt-8">
+                <span className="text-[#292929] bg-[#E8E4DE] shadow-lg content-center w-2/3 text-center text-2xl p-16 m-2">
+                For decades, those who identify as bi/multi-racial have been overlooked within society. Whether in families, neighborhoods, communities of faith, or otherwise, these individuals are often seen as "half" of one identity and "half" of another, and as such are rarely fully embraced. This "half existence" leads to bullying, disillusionment, poor mental health, and a host of other issues that do not reflect God's desires for their lives, which is all the more concerning when one considers that those who identify as bi/multi-racial will be the majority of people in the United States by 2040.
+                </span>
+                <span className="text-[#D6C6AA] bg-[#292929] shadow-lg content-center w-1/3 text-center text-xl m-2 p-10">
+                Two or More was created to help bring about a brighter future for community members and their families who represent two or more racial or ethnic backgrounds. This ministry creates spaces to explore the wholeness of whom these individuals are and not merely one part, established on the belief that racial reconciliation starts with knowing fully who one is in Christ, and continues by bringing awareness to all those to whom these people are connected.
+                </span>
+            </div>
+            <div className="flex justify-between bg-[#292929] p-5 shadow-lg">
                 <div className="m-10 items-center content-center">
+                    <h1 className="text-[#D6C6AA] text-4xl text-center pb-10">MEET BRIAN</h1>
                     <iframe src="https://player.vimeo.com/video/791915330?h=4e82b9fac6&color=f7542b& title=0&byline=0&portrait=0" 
                         width="640vh" 
                         height="360vh" 
@@ -18,27 +20,26 @@ function AboutUs(){
                         allowfullscreen
                     />
                 </div>
+                <div className="text-[#D6C6AA] text-2xl m-10">
+                    <div className="content-center m-5">
+                        <p className="text-[#E8E4DE]">
+                            Founded by Brian Petty, a multi-ethnic Black and Korean leader born and raised in Detroit, Michigan, Two or More provides social gatherings, immersion trips, and curriculum to help the bi/multi-ethnic individual, their children, and their families celebrate the fullness of their background. Brian uses both his years of study and personal life experience to develop all of the experiences, while also personally connecting with those that struggle to understand two or more ethnic identities within themselves. He believes that when these individuals learn to be undivided within and realizes that they are created wholly, bridge-building can truly begin. 
+                        </p>
+                    </div>
+                </div>
             </div>
             <div className="flex justify-between m-3 mt-8">
-                <div className="text-[#292929] bg-[#E8E4DE] content-center w-2/3  m-2 p-10">
+                <div className="text-[#292929] bg-[#E8E4DE] shadow-lg content-center w-2/3  m-2 p-10">
                     <h1 className="font-bold text-3xl text-center">Meet Karis</h1>
                     <h2 className="font-medium text-2xl text-center">Media Content and Connector Extrodinaire</h2>
                     <img src={'https://images.unsplash.com/photo-1457732815361-daa98277e9c8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'} alt="Whitney" className="rounded-full  w-52 h-52 float-left m-5"/>
                     <p className="m-10 text-xl text-end">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                 </div>
-                <div className="text-[#292929] bg-[#E8E4DE] content-center w-2/3 m-2 p-10">
+                <div className="text-[#292929] bg-[#E8E4DE] shadow-lg content-center w-2/3 m-2 p-10">
                     <h1 className="font-bold text-3xl text-center">Meet Whitney</h1>
                     <h2 className="font-medium text-2xl text-center">Education and Conversation Starter</h2>
                     <img src={'https://images.unsplash.com/photo-1457732815361-daa98277e9c8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'} alt="Whitney" className="rounded-full  w-52 h-52 float-right m-5"/>
                     <p className="m-10 text-xl text-start">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p> 
-                </div>
-            </div>
-            <div className="flex justify-between m-3 mt-8">
-                <div className="text-[#D6C6AA] bg-[#292929] content-center w-1/3 text-center text-2xl m-2 p-10">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                </div>
-                <div className="text-[#292929] bg-[#E8E4DE] content-center w-2/3 text-center text-2xl m-2 p-10">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
                 </div>
             </div>
             
